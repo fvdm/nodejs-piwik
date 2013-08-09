0ccd3e45ed01bde3d3f41dcc07f9f50af6fd8509
Franklin van de Meent <fr@nkl.in>
2013-08-09 23:33:12 +0200

    Clean up syntax

--- piwik.js
+++ piwik.js
@@ -6,52 +6,51 @@ Feedback:     https://github.com/fvdm/nodejs-piwik/issues
 License:      Unlicense / Public Domain (see UNLICENSE file)
 */
 
-// INIT
 var	urltool = require('url')
 var querystring = require('querystring')
 
-app.settings = {};
+var app = {settings: {}}
 
 // SETUP basics
 app.setup = function( baseURL, token ) {
-	var url = urltool.parse( baseURL, true );
+	var url = urltool.parse( baseURL, true )
 	
 	// protocol and port
 	switch( url.protocol ) {
 		case 'http:':
-			app.http = require('http');
-			app.settings.apiport = url.port ? url.port : 80;
-			break;
+			app.http = require('http')
+			app.settings.apiport = url.port ? url.port : 80
+			break
 		
 		case 'https:':
-			app.http = require('https');
-			app.settings.apiport = url.port ? url.port : 443;
+			app.http = require('https')
+			app.settings.apiport = url.port ? url.port : 443
 			break;
 	}
 	
 	// token in baseURL?
 	if( url.query && url.query.token_auth ) {
-		app.settings.token = url.query.token_auth;
+		app.settings.token = url.query.token_auth
 	}
 	
 	// override with custom token, if any
 	if( token ) {
-		app.settings.token = token;
+		app.settings.token = token
 	}
 	
 	// the rest
-	app.settings.apihost = url.hostname;
-	app.settings.apipath = url.pathname;
+	app.settings.apihost = url.hostname
+	app.settings.apipath = url.pathname
 }
 
 // API call
 app.api = function( vars, cb ) {
 	
 	// prepare fields
-	var vars = typeof vars == 'object' ? vars : {};
-	vars.module = 'API';
-	vars.format = 'JSON';
-	vars.token_auth = app.settings.token;
+	var vars = typeof vars == 'object' ? vars : {}
+	vars.module = 'API'
+	vars.format = 'JSON'
+	vars.token_auth = app.settings.token
 	
 	// do request
 	app.http.get(
@@ -63,20 +62,21 @@ app.api = function( vars, cb ) {
 		function( response ) {
 			
 			var data = '';
-			response.on( 'data', function( chunk ) { data += chunk });
+			response.on( 'data', function( chunk ) { data += chunk })
 			response.on( 'end', function() {
 				
 				// callback
+				data = data.trim()
 				if( data.substr(0,1) == '[' && data.substr(-1,1) == ']' ) {
-					cb( JSON.parse( data ) );
+					cb( JSON.parse( data ) )
 				}
 				
-			});
+			})
 			
 		}
-	);
+	)
 	
 }
 
 // ready
-module.exports = app;
+module.exports = app
