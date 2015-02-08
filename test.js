4c95669ab5448059916d9fe0a5fd9b7c6f221847
Franklin van de Meent <fr@nkl.in>
2015-02-08 15:08:46 +0000

    Test: clean code style, less complex (fixed #4)

--- test.js
+++ test.js
@@ -1,145 +1,146 @@
-var util = require('util')
+var util = require ('util');
 
 // Setup
 // $ env PIWIK_URL= PIWIK_TOKEN= PIWIK_SITEID= npm test
-var url = process.env.npm_config_piwikurl || process.env.PIWIK_URL || null
-var token = process.env.npm_config_piwiktoken || process.env.PIWIK_TOKEN || null
-var siteId = process.env.npm_config_piwiksiteid || process.env.PIWIK_SITEID || null
+var url = process.env.PIWIK_URL || null
+var token = process.env.PIWIK_TOKEN || null
+var siteId = process.env.PIWIK_SITEID || null
 
-var piwik = require('./').setup( url, token )
+var piwik = require ('./').setup (url, token);
 
 
 // handle exits
-var errors = 0
-process.on( 'exit', function() {
-  if( errors == 0 ) {
-    console.log('\n\033[1mDONE, no errors.\033[0m\n')
-    process.exit(0)
+var errors = 0;
+process.on ('exit', function () {
+  if (errors === 0) {
+    console.log ('\n\033[1mDONE, no errors.\033[0m\n');
+    process.exit (0);
   } else {
-    console.log('\n\033[1mFAIL, '+ errors +' error'+ (errors > 1 ? 's' : '') +' occurred!\033[0m\n')
-    process.exit(1)
+    console.log ('\n\033[1mFAIL, '+ errors +' error'+ (errors > 1 ? 's' : '') +' occurred!\033[0m\n');
+    process.exit (1);
   }
-})
+});
 
 // prevent errors from killing the process
-process.on( 'uncaughtException', function( err ) {
-  console.log()
-  console.error( err.stack )
-  console.trace()
-  console.log()
-  errors++
-})
+process.on ('uncaughtException', function (err) {
+  console.log ();
+  console.error (err.stack);
+  console.trace ();
+  console.log ();
+  errors++;
+});
 
 // Queue to prevent flooding
-var queue = []
-var next = 0
+var queue = [];
+var next = 0;
 
-function doNext() {
-  next++
-  if( queue[next] ) {
-    queue[next]()
+function doNext () {
+  next++;
+  if (queue [next]) {
+    queue [next] ();
   }
 }
 
 // doTest( passErr, 'methods', [
 //   ['feeds', typeof feeds === 'object']
 // ])
-function doTest( err, label, tests ) {
-  if( err instanceof Error ) {
-    console.error( label +': \033[1m\033[31mERROR\033[0m\n' )
-    console.error( util.inspect(err, false, 10, true) )
-    console.log()
-    console.error( err.stack )
-    console.log()
-    errors++
+function doTest (err, label, tests) {
+  if (err instanceof Error) {
+    console.error (label +': \033[1m\033[31mERROR\033[0m\n');
+    console.error (util.inspect (err, false, 10, true));
+    console.log ();
+    console.error (err.stack);
+    console.log ();
+    errors++;
   } else {
-    var testErrors = []
-    tests.forEach( function( test ) {
-      if( test[1] !== true ) {
-        testErrors.push(test[0])
-        errors++
+    var testErrors = [];
+    tests.forEach (function (test) {
+      if (test [1] !== true) {
+        testErrors.push (test [0]);
+        errors++;
       }
-    })
+    });
 
-    if( testErrors.length == 0 ) {
-      console.log( label +': \033[1m\033[32mok\033[0m' )
+    if(testErrors.length === 0) {
+      console.log (label +': \033[1m\033[32mok\033[0m');
     } else {
-      console.error( label +': \033[1m\033[31mfailed\033[0m ('+ testErrors.join(', ') +')' )
+      console.error (label +': \033[1m\033[31mfailed\033[0m ('+ testErrors.join(', ') +')');
     }
   }
 
-  doNext()
+  doNext ();
 }
 
 
 // ! Track one
-queue.push( function() {
-  piwik.track(
+queue.push (function () {
+  piwik.track (
     {
       idsite: siteId,
       url: 'https://www.npmjs.com/package/piwik',
-      cvar: { '1': ['node test', process.version] }
+      cvar: {'1': ['node test', process.version]}
     },
-    function( err, data ) {
-      doTest( err, 'track one', [
+    function (err, data) {
+      doTest (err, 'track one', [
         ['data type', typeof data === 'object'],
         ['data.status', data && data.status === 'success'],
         ['data.tracked', data && data.tracked === 1]
-      ])
+      ]);
     }
-  )
-})
+  );
+});
 
 // ! Track multi
-queue.push( function() {
-  piwik.track(
+queue.push (function () {
+  piwik.track (
     [
       {
         idsite: siteId,
         url: 'https://www.npmjs.com/package/piwik',
-        cvar: { '1': ['node test', process.version] }
+        cvar: {'1': ['node test', process.version]}
       },
       {
         idsite: siteId,
         url: 'https://github.com/fvdm/nodejs-piwik',
-        cvar: { '1': ['node test', process.version] }
+        cvar: {'1': ['node test', process.version]}
       }
     ],
-    function( err, data ) {
-      doTest( err, 'track multi', [
+    function (err, data) {
+      doTest (err, 'track multi', [
         ['data type', typeof data === 'object'],
         ['data.status', data && data.status === 'success'],
         ['data.tracked', data && data.tracked === 2]
-      ])
+      ]);
     }
-  )
-})
+  );
+});
 
 
 // ! API
-queue.push( function() {
-  piwik.api(
+queue.push (function () {
+  piwik.api (
     {
       method: 'Actions.getPageUrls',
       idSite: siteId,
       period: 'day',
       date: 'today'
     },
-    function( err, data ) {
-      doTest( err, 'api', [
+    function (err, data) {
+      doTest (err, 'api', [
         ['data type', data instanceof Array],
         ['data length', data && data.length >= 1],
         ['item type', data && data[0] instanceof Object],
         ['item label', data && data[0] && typeof data[0].label === 'string']
-      ])
-    })
-})
+      ]);
+    }
+  );
+});
 
 
 // Start the tests
-console.log('Running tests...\n')
-queue[0]()
+console.log ('Running tests...\n');
+queue [0] ();
 
-function output( err, data ) {
-  console.log( require('util').inspect( err || data, false, 10 ) )
+function output (err, data) {
+  console.log (require ('util').inspect (err || data, false, 10));
 }
