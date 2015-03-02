6fddd6bc53d0c546766b98a3b4d3c0025b6ee21a
Franklin van de Meent <fr@nkl.in>
2015-03-02 18:17:57 +0100

    More details in intro comment

--- piwik.js
+++ piwik.js
@@ -1,9 +1,11 @@
 /*
-Name:         piwik
-Description:  Access a Piwik API from node.js.
-Source:       https://github.com/fvdm/nodejs-piwik
-Feedback:     https://github.com/fvdm/nodejs-piwik/issues
-License:      Unlicense / Public Domain (see UNLICENSE file)
+Name:           piwik
+Description:    Access a Piwik API or track hits with node.js
+Author:         Franklin van de Meent (http://frankl.in)
+Source & docs:  https://github.com/fvdm/nodejs-piwik
+Feedback:       https://github.com/fvdm/nodejs-piwik/issues
+License:        Unlicense / Public Domain (see UNLICENSE file)
+                (https://github.com/fvdm/nodejs-piwik/raw/develop/UNLICENSE)
 */
 
 var urltool = require ('url');
