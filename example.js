f0602ec60e3dc7f43161f15a4353fa1a3140a689
Franklin van de Meent <fr@nkl.in>
2016-05-26 01:47:51 +0200

    fix(lint): Removed extra spaces in example.js

--- example.js
+++ example.js
@@ -4,10 +4,10 @@ var piwik = require ('piwik').setup ('https://example.tld/piwik/', 'abc123');
 // page urls for today
 piwik.api (
   {
-    method:   'Actions.getPageUrls',
-    idSite:   1,
-    period:   'day',
-    date:     'today'
+    method: 'Actions.getPageUrls',
+    idSite: 1,
+    period: 'day',
+    date: 'today'
   },
   console.log
 );
