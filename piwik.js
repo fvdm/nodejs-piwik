09d1522c5739151c03293697f73d926433b4c54b
Franklin <fr@nkl.in>
2021-08-31 00:18:56 +0200

    Breaking: Rewritten to modern standards

--- piwik.js
+++ piwik.js
@@ -7,307 +7,226 @@ License:        Unlicense (Public Domain, see UNLICENSE file)
                 (https://github.com/fvdm/nodejs-piwik/raw/develop/UNLICENSE)
 */
 
-var urltool = require ('url');
-var querystring = require ('querystring');
-var http = require ('httpreq');
-var app = {
-  settings: {
-    timeout: 5000,
-  },
-};
+const { stringify } = require ('querystring');
+const { doRequest } = require ('httpreq');
 
+module.exports = class MatomoAPI {
 
-/**
- * Callback an error
- *
- * @callback  callback
- * @param     {string}       msg       Error.message
- * @param     {mixed}        err       Error.error
- * @param     {number|null}  code      Error.code, i.e. `res.statusCode`
- * @param     {function}     callback  `function (error) {}`
- * @return    {void}
- */
+  /**
+   * Configuration
+   *
+   * @param   {object}  conf
+   * @param   {string}  endpoint        Base URL to Matomo
+   * @param   {string}  [token_auth]    API token, either set here or in the endpoint URL
+   * @param   {number}  [timeout=5000]  Request timeout in ms
+   */
 
-function callbackError (msg, err, code, callback) {
-  var error = new Error (msg);
+  constructor ({
+    endpoint,
+    timeout = 5000,
+    token_auth = null,
+  }) {
+    const url = new URL (endpoint);
+    const loc = url.origin + url.pathname.replace (/[^\/]+$/, '');
 
-  error.error = err;
-  error.code = code;
-  callback (error);
-}
-
-
-/**
- * Process talk() response
- *
- * @callback  callback
- * @param     {Error|null}  err         Response error
- * @param     {object}      res         Response resource
- * @param     {function}    [callback]  `(err, res)`
- * @returns   {void}
- */
-
-function processResponse (err, res, callback) {
-  var data = res && res.body || null;
-
-  if (err) {
-    callbackError ('request failed', err, null, callback);
-    return;
+    this._config = {
+      token_auth: url.searchParams.get ('token_auth') || token_auth,
+      endpoint: loc,
+      timeout,
+    };
   }
 
-  try {
-    data = JSON.parse (data);
 
-    if (data.result && data.result === 'error') {
-      callbackError ('api error', data.message, null, callback);
-      return;
-    }
-  }
-  catch (e) {
-    callbackError ('response invalid', e, null, callback);
-    return;
-  }
+  /**
+   * Process _talk() response
+   *
+   * @param   {object}  res  Response resource
+   *
+   * @return  {Promise<object>}
+   */
 
-  if (res && res.statusCode && res.statusCode >= 300) {
-    callbackError ('http error', data, res.statusCode, callback);
-    return;
-  }
+  async function _processResponse (res) {
+    try {
+      let error;
+      const data = JSON.parse (res.body);
 
-  if (callback) {
-    callback (null, data);
-  }
-}
-
-
-/**
- * API communication
- *
- * @callback  props.callback
- * @param     {object}        props                 Response resource
- * @param     {string}        [props.method=GET]    HTTP method: GET, POST
- * @param     {string}        [props.path=/]        Request path after hostname
- * @param     {number}        [props.timeout=5000]  Request time out in ms
- * @param     {object}        [props.query]         Data fields to send along
- * @param     {string}        [props.body]          POST JSON encoded body
- * @param     {function}      [props.callback]      `(err, res)`
- * @returns   {void}
- */
-
-function talk (props) {
-  var key;
-  var options = {
-    url: app.settings.baseURL + (props.path || ''),
-    method: props.method,
-    headers: {},
-    timeout: props.timeout || app.settings.timeout,
-  };
-
-  // build request
-  if (props.query instanceof Object) {
-    for (key in props.query) {
-      if (typeof props.query[key] === 'object') {
-        props.query[key] = JSON.stringify (props.query[key]);
+      if (data.result && data.result === 'error') {
+        throw new Error (data.message);
       }
     }
-  }
 
-  if (props.query) {
-    options.parameters = props.query;
-  }
-  else if (props.body) {
-    options.body = props.body;
-    options.headers['Content-Type'] = 'application/json';
-    options.headers['Content-Length'] = options.body.length;
-  }
+    if (res.statusCode >= 300) {
+      const error = new Error ('http error');
 
-  // send request
-  http.doRequest (options, function (err, res) {
-    processResponse (err, res, props.callback);
-  });
-}
-
-
-/**
- * Setup basics
- *
- * @param    {string}  baseURL                     Piwik URL
- * @param    {string}  [token=baseURL token_auth]  Piwik API token
- * @param    {number}  [timeout=5000]              Request time out in ms
- * @returns  {object}  app
- */
-
-function methodSetup (baseURL, token, timeout) {
-  var url = urltool.parse (baseURL, true);
-
-  app.settings.baseURL = url.protocol + '//' + url.host
-    + url.pathname.replace (/\/[^\/]+$/, '/');
-
-  // token in baseURL?
-  app.settings.token = url.query && url.query.token_auth || null;
-
-  // override with custom token, and set timeout
-  if (typeof token === 'number') {
-    timeout = token;
-  }
-  else if (token) {
-    app.settings.token = token;
-  }
-
-  app.settings.timeout = timeout || app.settings.timeout;
-  return app;
-}
-
-
-/**
- * API call
- *
- * @callback  callback
- * @param     {object}    [vars]      Parameters
- * @param     {function}  [callback]  `(err, res)`
- * @returns   {object}    app
- */
-
-function methodApi (vars, callback) {
-  vars = typeof vars === 'object' ? vars : {};
-  vars.module = 'API';
-  vars.format = 'JSON';
-  vars.token_auth = app.settings.token;
-
-  talk ({
-    method: 'GET',
-    path: 'index.php',
-    query: vars,
-    callback: callback || null,
-  });
-
-  return app;
-}
-
-
-/**
- * Convert tracking object to full querystring
- *
- * @param   {object}  obj  The tracking object
- * @return  {string}       Full querystring for request
- */
-
-function trackObject2request (obj) {
-  var keys = Object.keys (obj);
-  var key;
-  var val;
-  var i;
-
-  for (i = 0; i < keys.length; i++) {
-    key = keys[i];
-    val = obj[key];
-
-    if (typeof val === 'object') {
-      val = JSON.stringify (val);
+      error.data = data;
+      error.statusCode = res.statusCode;
+      throw error;
     }
 
-    obj[key] = val;
+    return data;
   }
 
-  obj.rec = 1;
-  obj.apiv = 1;
-  return '?' + querystring.stringify (obj);
-}
 
+  /**
+   * API communication
+   *
+   * @param   {object}  props
+   * @param   {string}  [props.method=GET]    HTTP method: GET, POST
+   * @param   {string}  [props.path='/']      Request path after hostname
+   * @param   {number}  [props.timeout=5000]  Request time out in ms
+   * @param   {object}  [props.query]         Data fields to send along
+   * @param   {object}  [props.body]          POST JSON encoded body
+   *
+   * @return  {Promise<object>}
+   */
 
-/**
- * Track one or multiple hits
- *
- * @callback  callback
- * @param     {object|array}  vars        Parameters or array with parameters objects
- * @param     {function}      [callback]  `(err, res)`
- * @returns   {object}        app
- */
+  async function _talk ({
+    method = 'GET',
+    path = '/',
+    query = null,
+    body = null,
+    timeout = this._config.timeout,
+  }) {
+    const options = {
+      url: this._config.endpoint + path,
+      headers: {},
+      method,
+      timeout,
+    };
 
-function methodTrack (vars, callback) {
-  var bulk = {
-    requests: [],
-  };
-  var i;
+    // build request
+    if (query) {
+      for (let key in query) {
+        if (typeof query[key] === 'object') {
+          query[key] = JSON.stringify (query[key]);
+        }
+      }
 
-  if (app.settings.token) {
-    bulk.token_auth = app.settings.token;
+      options.parameters = query;
+    }
+
+    if (body) {
+      options.body = body;
+      options.headers['Content-Type'] = 'application/json';
+      options.headers['Content-Length'] = body.length;
+    }
+
+    // send request
+    return doRequest (options)
+      .then (this._processResponse)
+    ;
   }
 
-  if (vars instanceof Array && vars[0] instanceof Object) {
+
+  /**
+   * Convert tracking object to full querystring
+   *
+   * @param   {object}  obj  The tracking object
+   *
+   * @return  {string}       Full querystring for request
+   */
+
+  async function _trackObject2request (obj) {
+    for (let key in obj) {
+      if (typeof obj[key] === 'object') {
+        obj[key] = JSON.stringify (obj[key]);
+      }
+    }
+
+    obj.rec = 1;
+    obj.apiv = 1;
+
+    return '?' + stringify (obj);
+  }
+
+
+  /**
+   * API call
+   *
+   * @param   {object}  [query]  Parameters
+   *
+   * @return  {Promise<object>}
+   */
+
+  async function api (query = {}) {
+    query.module = 'API';
+    query.format = 'JSON';
+    query.token_auth = this._config.token_auth;
+
+    return this._talk ({
+      method: 'GET',
+      path: 'index.php',
+      query,
+    });
+  }
+
+
+  /**
+   * Track one or multiple hits
+   *
+   * @param   {object|array}  vars  Parameters or array with parameters objects
+   *
+   * @return  {Promise<object>}
+   */
+
+  async function methodTrack (vars) {
+    const bulk = {
+      requests: [],
+    };
+
+    if (this._config.token_auth) {
+      bulk.token_auth = this._config.token_auth;
+    }
+
     // array with objects
-    for (i = 0; i < vars.length; i++) {
-      bulk.requests.push (trackObject2request (vars[i]));
+    if (vars instanceof Array && vars[0] instanceof Object) {
+      for (let i = 0; i < vars.length; i++) {
+        bulk.requests.push (this._trackObject2request (vars[i]));
+      }
     }
-  }
-  else if (vars instanceof Object) {
+
     // object
-    bulk.requests.push (trackObject2request (vars));
+    else if (vars instanceof Object) {
+      bulk.requests.push (this._trackObject2request (vars));
+    }
+
+    // send request
+    const data = await this._talk ({
+      method: 'POST',
+      path: 'matomo.php',
+      body: JSON.stringify (bulk),
+    });
+
+    // process result
+    if (data.status === 'success') {
+      return data;
+    }
+
+    const error = new Error ('track failed');
+
+    error.data = data;
+    throw error;
   }
 
-  talk ({
-    method: 'POST',
-    path: 'piwik.php',
-    body: JSON.stringify (bulk),
-    callback: function (err, data) {
-      if (err && callback) {
-        callback (err);
-        return;
-      }
 
-      if (data.status === 'success' && callback) {
-        callback (null, data);
-      }
-      else if (callback) {
-        callbackError ('track failed', data, null, callback);
-      }
-    },
-  });
+  /**
+   * Get spammers list from Github repo
+   *
+   * @return  {Promise<object>}
+   */
 
-  return app;
-}
+  async function methodLoadSpammers () {
+    return doRequest ({
+      url: 'https://github.com/piwik/referrer-spam-blacklist/raw/master/spammers.txt',
+      timeout,
+    })
+      .then (res => res.body.trim())
+      .then (data => data
+        .replace (/\s+\n/g, '\n')
+        .split ('\n')
+        .sort()
+      })
+    ;
+  }
 
-
-/**
- * Get spammers list from Github repo
- *
- * @callback  callback
- * @param     {function}  callback
- * @returns   {object}    app
- */
-
-function methodLoadSpammers (callback) {
-  var options = {
-    timeout: app.settings.timeout,
-  };
-
-  http.get (
-    'https://github.com/piwik/referrer-spam-blacklist/raw/master/spammers.txt',
-    options,
-    function (err, res) {
-      var data;
-
-      if (err) {
-        callback (err);
-      }
-      else {
-        data = res.body
-          .trim()
-          .replace (/\s+\n/g, '\n')
-          .split ('\n')
-          .sort();
-
-        callback (null, data);
-      }
-    },
-  );
-
-  return app;
-}
-
-
-// module
-app.setup = methodSetup;
-app.api = methodApi;
-app.track = methodTrack;
-app.loadSpammers = methodLoadSpammers;
-module.exports = app;
+};
